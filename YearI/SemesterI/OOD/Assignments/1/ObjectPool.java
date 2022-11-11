b5829fedf50968879e99b09df5778b3c3d066f89
Sudipto Ghosh <sudipto@jhatkaa.org>
2022-11-11 20:41:03 +0530

    ood: modify objectpool implementation

--- YearI/SemesterI/OOD/Assignments/1/ObjectPool.java
+++ YearI/SemesterI/OOD/Assignments/1/ObjectPool.java
@@ -9,13 +9,17 @@ import java.util.Random;
  *
  */
 public class ObjectPool {
+	private static int poolSize = 5;
 	private static ObjectPool _instance = null;
 	private static ArrayList<Object> pool = new ArrayList<>();
 	private static Random random = new Random();
 
 	private ObjectPool() {
 		random.setSeed(10);
-		System.out.println("ObjectPool<" + hashCode() + "> Created");
+		for (int i = 0; i < poolSize; i++) {
+			pool.add(new Object());
+		}
+		System.out.println("ObjectPool<" + hashCode() + "> Created with " + poolSize + " Objects");
 	}
 
 	public static ObjectPool getInstance() {
@@ -24,19 +28,29 @@ public class ObjectPool {
 		return _instance;
 	}
 
-	public Object fetch() {
+	public Object getObject() {
 		if (pool.size() > 0) {
 			int rnd = random.nextInt(pool.size());
 			Object obj = pool.get(rnd);
 			pool.remove(rnd);
+			setPoolSize(poolSize - 1);
 			return obj;
 		} else {
 			return null;
 		}
 	}
 
-	public void store(Object obj) {
+	public void storeObject(Object obj) {
 		pool.add(obj);
+		setPoolSize(poolSize + 1);
+	}
+
+	public int getPoolSize() {
+		return pool.size();
+	}
+
+	public void setPoolSize(int poolSize) {
+		ObjectPool.poolSize = poolSize;
 	}
 
 	/*
@@ -46,14 +60,7 @@ public class ObjectPool {
 		ObjectPool pool = ObjectPool.getInstance();
 
 		for (int i = 0; i < 5; i++) {
-			Object obj = new Object();
-			pool.store(obj);
-			System.out.println("Stored Object<" + obj.hashCode()
-					+ "> in ObjectPool<" + pool.hashCode() + ">");
-		}
-
-		for (int i = 0; i < 5; i++) {
-			Object obj = pool.fetch();
+			Object obj = pool.getObject();
 
 			if (obj == null) {
 				System.out.println("No Object Could Be Fetched From ObjectPool<"
@@ -61,8 +68,14 @@ public class ObjectPool {
 				continue;
 			}
 
-			System.out.println("Fetched Object<" + obj.hashCode()
-					+ "> from ObjectPool<" + pool.hashCode() + ">");
+			if (i % (random.nextInt(3) + 1) == 0) {
+				System.out.println("Object<" + obj.hashCode() + "> Fetched From ObjectPool<"
+						+ pool.hashCode() + "> (Objects Remaining: " + pool.getPoolSize() + ")");
+			} else {
+				pool.storeObject(obj);
+				System.out.println("Object<" + obj.hashCode() + "> Fetched and Stored Back Into ObjectPool<"
+						+ pool.hashCode() + "> (Objects Remaining: " + pool.getPoolSize() + ")");
+			}
 		}
 	}
 }
